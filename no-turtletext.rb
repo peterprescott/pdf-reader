490d57f01f26452d46b574d6459c9195493ca3cd
Peter Prescott <peter@prescott.ws>
2019-10-28 23:29:25 +0000

    pdf-reader can turn file to text (still disordered)

--- no-turtletext.rb
+++ no-turtletext.rb
@@ -2,9 +2,23 @@ require 'pdf-reader'
 
 reader = PDF::Reader.new("examplePDF.pdf")
 
-reader.pages.each do |page|
+#~ reader.pages.each do |page|
   #~ puts page.fonts
-  puts page.text
+  #~ puts page.text
   #~ puts page.raw_content
-end
+#~ end
+
+# open and write to a file with ruby
+open('myfile.out', 'w') { |f|
+	reader.pages.each do |page|
+	  #~ puts page.fonts
+	  #~ puts page.text
+	  #~ lines = page.text.split('n')
+	  #~ for line in lines do
+		
+        f.puts page.text
+	  #~ end
+
+	end
+}
 
