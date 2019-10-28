07e02e010a6ad345fdd1e61318b69f41d6b299c5
Peter Prescott <peter@prescott.ws>
2019-10-28 23:49:27 +0000

    get x coordinates of columns and use to try and print text column by column

--- start.rb
+++ start.rb
@@ -1,23 +1,95 @@
 require 'pdf-reader-turtletext'
+require 'pdf-reader'
 
 pdf_filename = 'examplePDF.pdf'
 reader = PDF::Reader::Turtletext.new(pdf_filename)
 options = { :y_precision => 5 }
 reader_with_options = PDF::Reader::Turtletext.new(pdf_filename,options)
 
-#~ text = reader.text_in_region(
-  #~ 10,   # minimum x (left-most)
-  #~ 900,  # maximum x (right-most)
-  #~ 200,  # minimum y (bottom-most)
-  #~ 400,  # maximum y (top-most)
-  #~ 1,    # page (default 1)
-  #~ false # inclusive of x/y position if true (default false)
-#~ )
 
-#~ puts text
 
 # find x,y coordinate of specific date
 
-text_by_exact_match = reader.text_position("December 22", 1)
 
-puts text_by_exact_match.class
+
+# firstc stands for first column
+firstc_text_by_exact_match = reader.text_position(/december 21/i, 1)
+
+firstc_x = firstc_text_by_exact_match[:x]
+
+# secondc stands for first column
+secondc_text_by_exact_match = reader.text_position(/december 25/i, 1)
+
+secondc_x = secondc_text_by_exact_match[:x]
+
+# secondc stands for first column
+thirdc_text_by_exact_match = reader.text_position(/december 13/i, 2)
+
+thirdc_x = thirdc_text_by_exact_match[:x]
+
+col_height = thirdc_text_by_exact_match[:y]
+
+#~ puts text_by_exact_match[:y]
+
+firstc_text1 = reader.text_in_region(
+  firstc_x,   # minimum x (left-most)
+  secondc_x,  # maximum x (right-most)
+  1,  # minimum y (bottom-most)
+  col_height, # maximum y (top-most)
+  1,    # page (default 1)
+  false # inclusive of x/y position if true (default false)
+)
+
+firstc_text2 = reader.text_in_region(
+  firstc_x,   # minimum x (left-most)
+  secondc_x,  # maximum x (right-most)
+  1,  # minimum y (bottom-most)
+  col_height, # maximum y (top-most)
+  2,    # page (default 1)
+  false # inclusive of x/y position if true (default false)
+)
+
+secondc_text1 = reader.text_in_region(
+  secondc_x,   # minimum x (left-most)
+  thirdc_x,  # maximum x (right-most)
+  1,  # minimum y (bottom-most)
+  col_height, # maximum y (top-most)
+  1,    # page (default 1)
+  false # inclusive of x/y position if true (default false)
+)
+
+
+secondc_text2 = reader.text_in_region(
+  secondc_x,   # minimum x (left-most)
+  thirdc_x,  # maximum x (right-most)
+  1,  # minimum y (bottom-most)
+  col_height, # maximum y (top-most)
+  2,    # page (default 1)
+  false # inclusive of x/y position if true (default false)
+)
+
+thirdc_text2 = reader.text_in_region(
+  thirdc_x,   # minimum x (left-most)
+  1000,  # maximum x (right-most)
+  1,  # minimum y (bottom-most)
+  col_height, # maximum y (top-most)
+  2,    # page (default 1)
+  false # inclusive of x/y position if true (default false)
+)
+
+
+puts firstc_x, secondc_x
+
+puts firstc_text1
+
+puts firstc_text2
+
+puts 'end first column'
+
+puts secondc_text1
+
+puts secondc_text2
+
+puts 'end second column'
+
+puts thirdc_text2
