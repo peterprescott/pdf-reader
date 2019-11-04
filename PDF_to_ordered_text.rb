e9ed590a30a2fb66b3df17af023b3068e6c4a3b0
Peter Prescott <peter@prescott.ws>
2019-11-04 21:41:27 +0000

    begin generalising for other pdfs -- but we now lose spaces (for 2.pdf)!

--- PDF_to_ordered_text.rb
+++ PDF_to_ordered_text.rb
@@ -1,45 +1,68 @@
 require 'pdf-reader'
 
-reader = PDF::Reader.new("examplePDF.pdf")
 
 
-# write each page to a file
-i = 1
-reader.pages.each do |page|
+def page_to_text(file)
+    reader = PDF::Reader.new(File.join("pdfs",file))
 
-	filename = "page#{i}.txt"
-	open(filename, 'w') { |f|
-		
-		f.puts page.text
-        
-	}
-	i = i + 1
+    # write each page to a file
+    i = 1
+    reader.pages.each do |page|
+
+        filename = "page#{i}.txt"
+        open(filename, 'w') { |f|
+            
+            f.puts page.text
+            
+        }
+        i = i + 1
+    end
+
 end
 
+def order_pdf(file)
+    reader = PDF::Reader.new(File.join("pdfs",file))
 
+    # write each page to a file
+    i = 1
+    reader.pages.each do |page|
 
-open('ordered.txt', 'w') { |f|
-	# start with the first column of the second page
-	File.open("page2.txt", "r").each_line do |line|
-		f.puts line[0..65]
-	end
-	# ... second column of second page
-	File.open("page2.txt", "r").each_line do |line|
-		f.puts line[64..130]
-	end
-	# ... third column of second page
-	File.open("page2.txt", "r").each_line do |line|
-		f.puts line[130..-1]
-	end
-	# ... then first column of first page
-	File.open("page1.txt", "r").each_line do |line|
-		f.puts line[0..65]
-	end
-	# ... finally second column of second page
-	File.open("page1.txt", "r").each_line do |line|
-		f.puts line[64..130]
-	end
-}
+        filename = "page#{i}.txt"
+        open(filename, 'w') { |f|
+            
+            f.puts page.text
+            
+        }
+        i = i + 1
+    end
 
+    output_filename = file[0..-5] + '_reordered' + '.txt'
+
+    open(File.join('output',output_filename), 'w') { |f|
+        # start with the first column of the second page
+        File.open("page2.txt", "r").each_line do |line|
+            f.puts line[0..65]
+        end
+        # ... second column of second page
+        File.open("page2.txt", "r").each_line do |line|
+            f.puts line[64..130]
+        end
+        # ... third column of second page
+        File.open("page2.txt", "r").each_line do |line|
+            f.puts line[130..-1]
+        end
+        # ... then first column of first page
+        File.open("page1.txt", "r").each_line do |line|
+            f.puts line[0..65]
+        end
+        # ... finally second column of second page
+        File.open("page1.txt", "r").each_line do |line|
+            f.puts line[64..130]
+        end
+    }
+end
+
+# order_pdf("1.pdf")
+page_to_text("2.pdf")
 
 
